4796729569b9abba9484123b55dc19fad669216a
shadd0w <rene.gmitter@wame.sk>
2016-05-03 10:57:51 +0200

    Fix meta keywords in head

--- MetaKeywords.php
+++ MetaKeywords.php
@@ -2,12 +2,11 @@
 
 namespace Wame\HeadControl;
 
-use Wame\Utils\Strings;
 use Wame\HeadControl\IMetaType;
 
 class MetaKeywords extends MetaType implements IMetaType
 {
 	public function render() {
-		return '<meta name="keywords" content="' . Strings::truncate($this->content, 150, null) . '">';
+		return '<meta name="keywords" content="' . $this->content . '">';
 	}
 }
