f8a5b3aac0eb61badc3893463ab8ed3b733ab902
shadd0w <rene.gmitter@wame.sk>
2016-05-03 10:58:19 +0200

    Move parse logic out of component

--- MetaType.php
+++ MetaType.php
@@ -2,8 +2,6 @@
 
 namespace Wame\HeadControl;
 
-use Nette\Neon\Neon;
-
 class MetaType
 {
 	/** @var string */
@@ -20,23 +18,5 @@ class MetaType
 	public function setContent($content)
 	{
 		$this->content = $content;
-	}
-	
-	/**
-	 * Parse neon string to array
-	 * 
-	 * @return array	values
-	 */
-	private function parse()
-	{
-		$seo = $this->seoRepository->get($criteria);
-		
-		try {
-			$values = Neon::decode($seo->content);
-			
-			return $values;
-		} catch(Nette\Neon\Exception $e) {
-			$this->flashMessage($e, 'error');
-		}
 	}
 }
